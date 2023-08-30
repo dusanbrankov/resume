5ab749644d043ab5c3b78e5d1701f1c50ebd027d
Dušan Brankov <dusanbrankov@proton.me>
2023-08-30 14:38:53 +0200

    Rename 'main.scss' to 'index.scss'

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './assets/styles/main.scss';
+import './assets/styles/index.scss';
 import App from './App.jsx';
 import './i18n.js';
 
