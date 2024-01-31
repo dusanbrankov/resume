362544d7eaf3decfe8698b339f2b4468b2c08398
Dušan Brankov <dusanbrankov@proton.me>
2024-01-31 11:21:21 +0100

    Add social links in site footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,18 @@ import DownloadButton from "./DownloadButton";
 function Footer() {
   return <>
     <footer className="site-footer">
-      <a href="https://github.com/dusanbrankov/resume">GitHub</a>
+      <div className="social-links">
+        <a
+          href="https://github.com/dusanbrankov/"
+          target="_blank"
+          rel="noopener"
+        >GitHub</a>
+        <a
+          href="https://www.linkedin.com/in/dusanbrankov/"
+          target="_blank"
+          rel="noopener"
+        >LinkedIn</a>
+      </div>
       <DownloadButton />
     </footer>
   </>;
